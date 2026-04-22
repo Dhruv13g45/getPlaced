a1067fb4381055a28a245ed87d6f367e2f2b6b72
yadav-sundarm <sundaramy356504@gmail.com>
2026-04-22 21:47:30 +0530

    test commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,30 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-<<<<<<< HEAD
-import mongoose from "mongoose";
-import cors from "cors";
-import {createServer} from "node:http";
-import {Server}  from "socket.io";
-
-dotenv.config();
-const app = express();
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("MongoDB Atlas connected");
-  } catch (error) {
-    console.error("MongoDB connection failed", error);
-  }
-};
-
-connectDB();
-
-app.use(cors());
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-=======
 import cors from "cors";
 import { createServer } from "http";
 import { connectToServer } from "./controllers/socketManager.js";
@@ -83,4 +58,3 @@ const startServer = async () => {
 };
 
 startServer();
->>>>>>> da345dad347d4bde3361433395c0eabfc5f76761
